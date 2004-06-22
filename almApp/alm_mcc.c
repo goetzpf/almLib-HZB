2044a845f4d5547ce2af48f7d4af1bbe60ceef34
luchini <unknown>
2004-06-22 09:27:06 +0000

    moved bsp specific routines to almLib.c & visa-versa

--- almApp/alm_mcc.c
+++ almApp/alm_mcc.c
@@ -1,4 +1,5 @@
-/* $RCSfile: alm_mcc.c,v $ */
+static char RcsId[]      =
+"@(#)$Id: alm_mcc.c,v 2.5 2004/06/22 09:27:06 luchini Exp $";
 
 /*+**************************************************************************
  *
@@ -6,7 +7,7 @@
  *
  * Module:	Alm - High Resolution Timer and Alarm Clock Library
  *
- * File:	alm_mcc.c  -  (included by alm.c)
+ * File:	alm_mcc.c
  *
  * Description:	Library package to implement a high resolution alarm clock.
  *              System dependent part - Motorola MCCchip
@@ -14,12 +15,22 @@
  *                Uses the timer 4 on the mvme16x's MCC chip. (See also 
  *		  timestamp timer code in ~vw/src/drv/timer/mccTimerTS.c)
  *
+ *             The software for the decrementer timer chip (MPIC) found on
+ *             most powerPc processors is located in the file alm_ppcDec.c
+ *
+ *             Debug messages are used in the macro ALM_DEBUG_MSG().
+ *             This macro is defined to be empty if the definition
+ *                   INCLUDE_ALM_DEBUG
+ *             has not been defined prior to alm_debug.h. This
+ *             definition can be included in the makefile during 
+ *             compilation.
+ 
  * Author(s):	Ralph Lange
  *
- * $Revision: 2.4 $
- * $Date: 2004/03/29 13:29:14 $
+ * $Revision: 2.5 $
+ * $Date: 2004/06/22 09:27:06 $
  *
- * $Author: lange $
+ * $Author: luchini $
  *
  * Revision log at end of file
  *
@@ -53,67 +64,30 @@
  *
  **************************************************************************-*/
 
-#include <vxWorks.h>
-#include <intLib.h>
-#include <iv.h>
-
-#include <drv/multi/mcchip.h>
-
-#include <debugmsg.h>
-#include <almLib.h>
-
-				/* Definitions for 25 MHz CPU */
-#define INT_INHIBIT_25  25ul	/* Minimum interrupt delay */
-#define INT_SUBTRACT_25 46ul	/* Subtracted from every delay */
-
-				/* Definitions for 32 MHz CPU */
-#define INT_INHIBIT_32 20ul	/* Minimum interrupt delay */
-#define INT_SUBTRACT_32 34ul	/* Subtracted from every delay */
-
-                                /* Safe values (prelimininary patch) */
-#define INT_INHIBIT_SAFE 50ul
-#define INT_SUBTRACT_SAFE 0ul
-
-				/* Magic Words */
-#define MAGIC_P  (unsigned long*)(0xfffc1f28)	/* Address of Magic Word */
-#define MAGIC_25 0x32353030ul	/* ASCII 2500 */
-#define MAGIC_32 0x33323030ul	/* ASCII 3200 */
+#define INCLUDE_ALM_DEBUG      /* for ALM_DEBUG_MSG() macro w/logMsg()   */
+#include <vxWorks.h>           /* for OK and ERROR                       */
+#include <intLib.h>            /* for intLock(),intUnlock()              */
+#include <logLib.h>            /* for logMsg()                           */
+#include <alm.h>               /* for  alm_status_ts                     */
+#include <alm_mcc.h>           /* for MCC_BASE_ADRS,MCC_INT_VEC_BASE,etc */
+                               /* for MAGIC_P,MAGIC_25,MAGIC_32,etc      */
+#include <alm_debug.h>         /* for ALM_DEBUG_MSG()                    */
+#include <alm_debug.c>         /* for alm_debug_msg(),alm_set_debug_msg()*/
 
 
 /*+**************************************************************************
- *		Global Functions implemented here
+ *	       Local variables
  **************************************************************************-*/
-				/* These are declared in almLib.h */
-#if 0
-/* Return a time stamp */
-unsigned long alm_get_stamp (void);
-
-/* Initialize the alarm library */
-long almInit (void);
-
-/* Return the frequency of the alarm clock */
-unsigned long alm_freq (void);
-#endif
+static unsigned long  int_inhibit=0; 
+static unsigned long  int_subtract=0;
 
 /*+**************************************************************************
- *		Local Functions
+ *	       Global variables - (see almLib.c)
  **************************************************************************-*/
+alm_status_ts        alm_status = {FALSE,FALSE,FALSE,FALSE,0,
+                                   1,TIMER4_INT_VEC,
+                                   FALSE,FALSE,0};
 
-/* Setup the timer to interrupt in <delay> microseconds */
-static void alm_setup (unsigned long delay);
-
-/* Disable the timer interrupts */
-static void alm_disable (void);
-
-/* The alarm counter's interrupt handler */
-static void alm_int_handler (int arg);
-
-/*+**************************************************************************
- *		Globals
- **************************************************************************-*/
-
-static unsigned long int_inhibit;
-static unsigned long int_subtract;
 
 
 /*+**************************************************************************
@@ -121,7 +95,8 @@ static unsigned long int_subtract;
  * Function:	alm_get_stamp
  *
  * Description:	Returns a time stamp from the alarm timer (32 bit;
- *              1 us resolution)
+ *              1 us resolution). Please note that this timer is 
+ *              read and writable at anytime.
  *
  * Arg(s) In:	None.
  *
@@ -133,9 +108,9 @@ static unsigned long int_subtract;
 
 unsigned long alm_get_stamp (void)
 {
+   /* Read the tick timer counter register */
    return(*MCC_TIMER4_CNT);
 }
-
 
 /*+**************************************************************************
  *
@@ -154,26 +129,78 @@ unsigned long alm_get_stamp (void)
 
 void alm_setup (unsigned long delay)
 {
-   register int lock_key;
+   register int lock_key=0;
 
-   DBG(5, "Entering alm_setup.");
+   ALM_DEBUG_MSG(5,"alm_setup: Entering function.",0,0);
 
-   lock_key = intLock();	/* Lock interrupts */
+   lock_key = intLock();	/* Lock interrupts             */
+   alm_status.running = TRUE;	/* Set running flag            */
 
-   status.running = TRUE;	/* Set running flag */
+   /* The timer has a resolutin of 1microsecond. When free running, 
+    * it will roll over every 71.6 minutes.  
+    * 
+    * Each timer has a 
+    *   32-bit counter register
+    *   32-bit compare register
+    *    4-bit overflow register
+    *    enable bit
+    *    overflow clear bit, 
+    *    clear-on-compare enable bit
+    * The counter is readable and writeable at any time. 
+    * When enabled in free-run mode, it increments every 1usec.
+    * When the counter is enabled in clear-on-compare mode, it increments
+    * every 1usec until the counter value matches the value in the compare
+    * register.  When a match occurs, the counter is cleared. When a match
+    * occurs in either mode, an interrupt is sent to the local bus interrupter
+    * and the overflow counter is incremented. An interrupt to the local bus 
+    * is only generated if the tick timer interrupt is enabled by the local
+    * bus interrupter. The overflow counter can be cleared by writing a 1
+    * to the overflow clear bit.
+    */
 
-				/* Set the timer compare value */
-   *MCC_TIMER4_CMP = *MCC_TIMER4_CNT + 
-      (delay < INT_INHIBIT ? INT_INHIBIT : delay);
+    /* load compare register with the number of micro seconds */
+   *MCC_TIMER4_CMP = *MCC_TIMER4_CNT +
+                      (delay < int_inhibit ? int_inhibit : delay);
 
-				/* Reset & enable the alarm timer interrupt */
-   *MCC_T4_IRQ_CR  = T4_IRQ_CR_IEN | T4_IRQ_CR_ICLR | ALARMCLOCK_LEVEL;
+   /* Load tick timer interrupt control register to
+    * enable interrupts, clear any pending interrupts, 
+    * and set the IRQ level.
+    */
+   *MCC_T4_IRQ_CR  = T4_IRQ_CR_IEN  | 
+                     T4_IRQ_CR_ICLR |
+                     alm_status.int_pri;
 
    intUnlock(lock_key);		/* Unlock interrupts */
 
-   PRF(4, ("alm_setup: counter set to %u ticks.\n", delay));
-   DBG(5, "Leaving alm_setup.");
+   ALM_DEBUG_MSG(4,"alm_setup: Counter set to %u ticks.\n",delay,0);
+   ALM_DEBUG_MSG(5,"alm_setup: Leaving function.",0,0);
    return;
+}
+
+
+/*+**************************************************************************
+ *
+ * Function:	alm_reset
+ *
+ * Description:	This function clears the tick timer counter.
+ *
+ * Arg(s) In:	enb - flag to enable (1) or disable counting.
+ *
+ * Arg(s) Out:	None.
+ *
+ * Side: Always enable counting.
+ *
+ * Return(s):	None.
+ *
+ **************************************************************************-*/
+
+long alm_reset(int enb)
+{
+
+  *MCC_TIMER4_CNT = 0; /* Set the tick timer counter register */
+  if (enb)
+    *MCC_TIMER4_CR = TIMER4_CR_CEN;    /* enable counting   */
+  return(OK); 
 }
 
 
@@ -194,73 +221,34 @@ void alm_setup (unsigned long delay)
 
 void alm_disable (void)
 {
-   register int lock_key;
+   register int lock_key=0;
 
-   DBG(5, "Entering alm_disable.");
+   ALM_DEBUG_MSG(5, "alm_disable: Entering function.",0,0);
 
-   if (status.running) {
+   if (alm_status.running) {
+      lock_key = intLock();	           /* Lock interrupts                */
+      /*
+       * Clear the tick timer interrupt control register
+       * to disable interrupts. Note: clearing the counter
+       * enable bit will also disable the counter. However,
+       * it is nicer to simply reset this register.
+       */
+      *MCC_T4_IRQ_CR = 0;                  /* clear the counter enable bit   */
+      alm_status.running = FALSE;          /* indicate status at NOT running */
+      intUnlock(lock_key);	           /* Unlock interrupts              */
 
-      lock_key = intLock();	/* Lock interrupts */
-
-      *MCC_T4_IRQ_CR = 0;	/* Disable interrupts */
-
-      status.running = FALSE;
-
-      intUnlock(lock_key);	/* Unlock interrupts */
-
-      DBG(4, "alm_disable: counter interrupts disabled.");
+      ALM_DEBUG_MSG(4,"alm_disable: Counter interrupts disabled.",0,0);
    }
-
-   DBG(5, "Leaving alm_disable.");
-}
-
-
-/*+**************************************************************************
- *
- * Function:	alm_int_handler
- *
- * Description:	Interrupt handler (gets called at counter overflow).
- *              The counter is disabled and a user routine is called (if it 
- *              was connected by alm_start).
- *
- * Arg(s) In:	arg  -  Standard interrupt handler argument.
- *
- * Arg(s) Out:	None.
- *
- * Return(s):	None.
- *
- **************************************************************************-*/
-
-void alm_int_handler (int arg)
-{
-   register int lock_key;
-
-   DBG_ENTER_INT_CONTEXT;
-
-   lock_key = intLock();	/* Lock interrupts */
-
-   *MCC_T4_IRQ_CR |= T4_IRQ_CR_ICLR; 	/* Acknowledge timer interrupt */
-
-   intUnlock(lock_key);		/* Unlock interrupt */
-
-   if (status.in_use) {		/* User call is active */
-
-      status.woke_up = TRUE;
-      DBG(3, "Handler woke up.");
-
-   } else			/* No users around */
-
-      alm_check();		/* Check for and post alarms */
-
-   DBG_LEAVE_INT_CONTEXT;
+   ALM_DEBUG_MSG(5, "alm_disable: Leaving function.",0,0);
+   return;
 }
 
 
 /*+**************************************************************************
  *
- * Function:	almInit
+ * Function:	alm_enable
  *
- * Description:	Initialize the alarm library.
+ * Description:	Enable the timer.
  *
  * Arg(s) In:	None.
  *
@@ -271,50 +259,38 @@ void alm_int_handler (int arg)
  *
  **************************************************************************-*/
 
-long almInit (void)
+long alm_enable(void)
 {
-static char
-rcsid[] = "@(#)almLib: $Id: alm_mcc.c,v 2.4 2004/03/29 13:29:14 lange Exp $";
+     long status=OK;
 
-   DBG(5, "Entering almInit.");
+     ALM_DEBUG_MSG(5, "alm_enable: Entering function.",0,0);
 
-   if (!status.init_d) {
-      status.init_d = TRUE;
+     /* Set the control register to enable the timer. */
+     *MCC_TIMER4_CR  = TIMER4_CR_CEN;  /* Set the counter enable bit */
 
-      DBG_INIT;
+     /* Check for CPU speed */
+     if (*(MAGIC_P) == MAGIC_25) {
+       int_inhibit  = INT_INHIBIT_25;
+       int_subtract = INT_SUBTRACT_25;
+     }
+     else if (*(MAGIC_P) == MAGIC_32) {
+       int_inhibit  = INT_INHIBIT_32;
+       int_subtract = INT_SUBTRACT_32;
+     } else {
+        ALM_DEBUG_MSG(1,"alm_enable: Illegal CPU speed.",0,0);
+        status = ERROR;
+     }
+     /* 
+      * ??? Secure PATCH for T2.0.2
+      * should be replaced by something real !!!
+      */
+     if (status==OK) {    
+       int_inhibit  = INT_INHIBIT_SAFE;
+       int_subtract = INT_SUBTRACT_SAFE;
+     }
 
-      if (INIT_LOCK(alm_lock)) { /* Init mutex semaphore */
-	 return ERROR;
-      }
-				/* Connect interrupt handler */
-      (void) intConnect (INUM_TO_IVEC (MCC_INT_VEC_BASE + MCC_INT_TT4),
-			 alm_int_handler, NULL);
-
-				/* Enable the timer */
-      *MCC_TIMER4_CR  = TIMER4_CR_CEN;
-
-				/* Check for CPU speed */
-      if (*(MAGIC_P) == MAGIC_25) {
-	 int_inhibit  = INT_INHIBIT_25;
-	 int_subtract = INT_SUBTRACT_25;
-      }
-      else if (*(MAGIC_P) == MAGIC_32) {
-	 int_inhibit  = INT_INHIBIT_32;
-	 int_subtract = INT_SUBTRACT_32;
-      } else {
-	 DBG(1, "alm: Illegal CPU speed.");
-	 return ERROR;
-      }
-
-/* ??? Secure PATCH for T2.0.2 - should be replaced by something real !!! */
-	 int_inhibit  = INT_INHIBIT_SAFE;
-	 int_subtract = INT_SUBTRACT_SAFE;
-
-      DBG(1, "alm: initialization done.");
-   }
-
-   DBG(5, "Leaving almInit.");
-   return OK;
+     ALM_DEBUG_MSG(5,"alm_enable: Leaving function.",0,0);
+     return (status);
 }
 
 
@@ -335,63 +311,92 @@ rcsid[] = "@(#)almLib: $Id: alm_mcc.c,v 2.4 2004/03/29 13:29:14 lange Exp $";
 
 unsigned long alm_freq (void)
 {
-   unsigned long timer_freq;
-   unsigned long bclk;
+   unsigned long timer_freq=0;     /* clock frequency                */
+   unsigned long bclk = 25000000;  /* default cpu clock speed, 25Mhz */
 
-   DBG(5, "Entering alm_freq.");
+   ALM_DEBUG_MSG(5,"alm_freq: Entering function.",0,0);
 
-   if (*MCC_VERSION_REG & VERSION_REG_SPEED)	/* check clock speed */
-      bclk = 33000000;
-   else
-      bclk = 25000000;
-
+   if (*MCC_VERSION_REG & VERSION_REG_SPEED)	/* check clock speed        */
+      bclk = 33000000;                          /* set clock speed to 33Mhz */
    timer_freq = bclk/(256 - (*MCC_PRESCALE_CLK_ADJ & 0xff));
 
-   DBG(5, "Leaving alm_freq.");
+   ALM_DEBUG_MSG(5,"alm_freq: Leaving function.",0,0);
    return (timer_freq);
 }
 
+
 /*+**************************************************************************
  *
- * Project:	Experimental Physics and Industrial Control System (EPICS)
+ * Function:	alm_int_ack
  *
- * Module:	Alm - High Resolution Timer and Alarm Clock Library
+ * Description:	This function is called in the user defined
+ *              interrupt handler to acknowledge the interrupt.
  *
- * File:	alm_mcc.c  -  (included by alm.c)
+ * Arg(s) In:	None
  *
- * Description:	Library package to implement a high resolution alarm clock.
- *              System dependent part - Motorola MCCchip
- *              On the MOTOROLA MVME 162/167:
- *                Uses the timer 4 on the mvme16x's MCC chip. (See also 
- *		  timestamp timer code in ~vw/src/drv/timer/mccTimerTS.c)
+ * Arg(s) Out:	None.
  *
- * Author(s):	Ralph Lange
- *
- * $Log: alm_mcc.c,v $
- * Revision 2.4  2004/03/29 13:29:14  lange
- * Bugfix: Guard bitfield operations
- *
- * Revision 2.3  1999/09/08 17:40:22  lange
- * Fixed Tornado101 warnings
- *
- * Revision 2.2  1997/07/31 18:07:07  lange
- * alm -> almLib
- *
- * Revision 2.1  1997/07/31 17:50:25  lange
- * alm_init -> almInit, reflects mv162 cpu speed.
- *
- * Revision 2.0  1997/02/07 16:30:49  lange
- * Changed interface; alm is standalone module now.
- *
- * Revision 1.2  1997/02/07 16:04:36  lange
- * Added counter increment; made alm a module of its own.
- *
- * Revision 1.1  1996/10/29 13:11:53  lange
- * First version to go into EPICS tree (locally).
- *
- * Copyright (c) 1996, 1997, 1999
- *			Berliner Elektronenspeicherring-Gesellschaft
- *			      fuer Synchrotronstrahlung m.b.H.,
- *				     Berlin, Germany
+ * Return(s):	None.
  *
  **************************************************************************-*/
+
+void alm_int_ack(void)
+{
+   register int lock_key=0;
+     
+   /* Acknowledge the interrupt. */
+   lock_key = intLock();   
+   *MCC_T4_IRQ_CR |= T4_IRQ_CR_ICLR;  /* Clear IRQ's */
+   intUnlock(lock_key);	  
+   return;
+}
+
+
+/*+**************************************************************************
+ * 
+ * Function:    alm_intLevelGet
+ *
+ * Description:  This routine returns interrupt vector priority level.
+ *              currently set.
+ *
+ * Arg(s) In:   None
+ * 
+ * Arg(s) Out:  None.
+ *
+ * Return(s):   Interrupt vector priority level (1-7).
+ *
+ **************************************************************************-*/
+ 
+int alm_intLevelGet(void)
+{
+    unsigned long level = 0;
+       
+   /* Read the tick timer interrupt control register 
+    * and get the current IRQ priority level set.
+    */
+   level = *MCC_T4_IRQ_CR & 0xf;
+   return((int)level);
+}
+
+
+/*+**************************************************************************
+ *
+ * Function:	alm_int_delay_adjust
+ *
+ * Description:	This function is called during alarm initialzation
+ *              by the user to obtain the interrupt dealy adjustment.
+ *
+ * Arg(s) In:	None
+ *
+ * Arg(s) Out:	None.
+ *
+ * Return(s):   interrupt delay adjustment value
+ *
+ **************************************************************************-*/
+
+unsigned long alm_int_delay_adjust(void)
+{
+    return(int_subtract);
+}
+
+
