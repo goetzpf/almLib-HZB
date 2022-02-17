038f726a570330092d999828152d7f5cbfcc5df4
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2022-02-17 22:19:16 +0000

    alarm is now published under license GPL v.3.

--- src/almLib.h
+++ src/almLib.h
@@ -1,43 +1,29 @@
-/*+**************************************************************************
- *
- * Project:     Experimental Physics and Industrial Control System (EPICS)
- *
- * Module:      almLib - High Resolution Timer and Alarm Clock Library
- *
- * File:        almLib.h
- *
- * Description: Library package to implement a high resolution alarm clock.
- *              Built upon low-level timerLib (see module bspDep).
- *
- * This software is copyrighted by the BERLINER SPEICHERRING-
- * GESELLSCHAFT FUER SYNCHROTRONSTRAHLUNG M.B.H., BERLIN, GERMANY.
- * The following terms apply to all files associated with the software.
- *
- * BESSY hereby grants permission to use, copy and modify this software
- * and its documentation for non-commercial, educational or research
- * purposes, provided that existing copyright notices are retained in
- * all copies.
- *
- * The receiver of the software provides BESSY with all enhancements,
- * including complete translations, made by the receiver.
- *
- * IN NO EVENT SHALL BESSY BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
- * SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE
- * OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF, EVEN
- * IF BESSY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * BESSY SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NON-INFRINGEMENT. THIS SOFTWARE IS PROVIDED
- * ON AN "AS IS" BASIS, AND BESSY HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
- * SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS.
- *
- * Copyright (c) 1996, 1997, 1999
- *                      Berliner Elektronenspeicherring-Gesellschaft
- *                            fuer Synchrotronstrahlung m.b.H.,
- *                                   Berlin, Germany
- *
- **************************************************************************-*/
+/*==========================================================
+                             alarm
+  ==========================================================
+
+Copyright 2022 Helmholtz-Zentrum Berlin für Materialien und Energie GmbH
+<https://www.helmholtz-berlin.de>
+
+This file is part of the alarm EPICS support module.
+
+Module     : almLib - High Resolution Timer and Alarm Clock Library
+Description: Library package to implement a high resolution alarm clock.
+             Built upon low-level timerLib (see module bspDep).
+
+alarm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+alarm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with alarm.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #ifndef ALMLIB_H
 #define ALMLIB_H
